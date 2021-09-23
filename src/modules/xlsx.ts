2603bd1883fa562c910ee0cabd3feae63a796a45
olivierbretaud <olivierbretaud@hotmail.fr>
2021-09-23 10:45:39 +0200

    feat(Parse xslx)

--- src/modules/xlsx.ts
+++ src/modules/xlsx.ts
@@ -1,42 +1,48 @@
+/* eslint-disable no-continue */
 const XLSX = require('xlsx');
 
 exports.xlsxToJs = (filePath: string) => {
   const workbook = XLSX.readFile(filePath);
   const sheetNameList = workbook.SheetNames;
 
-  const sheets: any = {};
+  const sheets: object[] = XLSX.utils.sheet_to_json(
+    workbook.Sheets[sheetNameList[0]]
+  );
 
-  sheetNameList.forEach((y: string) => {
-    // const worksheet = workbook.Sheets[y];
-    // const headers = {};
-    const data: any[] = [];
-    // for (z in worksheet) {
-    //   if (z[0] === '!') continue;
-    //   // parse out the column, row, and value
-    //   let tt = 0;
-    //   for (let i = 0; i < z.length; i++) {
-    //     if (!isNaN(z[i])) {
-    //       tt = i;
-    //       break;
-    //     }
-    //   }
-    //   const col = z.substring(0, tt);
-    //   const row = parseInt(z.substring(tt));
-    //   const value = worksheet[z].v;
-
-    //   // store header names
-    //   if (row == 1 && value) {
-    //     headers[col] = value;
-    //     continue;
-    //   }
-
-    //   if (!data[row]) data[row] = {};
-    //   data[row][headers[col]] = value;
-    // }
-    // drop those first two rows which are empty
-    data.shift();
-    data.shift();
-    sheets[y] = data;
+  sheets.forEach((r) => {
+    console.log(r);
   });
+
+  // sheetNameList.forEach((y: string) => {
+  //   const worksheet = workbook.Sheets[y];
+  //   const headers: any = {};
+  //   const data: any[] = [];
+  //   Object.keys(worksheet).forEach((z: any) => {
+  //     // if (z[0] === '!') break;
+  //     // parse out the column, row, and value
+  //     let tt: number = 0;
+  //     for (let i: number = 0; i < z.length; i += 1) {
+  //       if (!Number.isNaN(z[i])) {
+  //         tt = i;
+  //       }
+  //     }
+
+  //     const col: string = z[0];
+  //     const row: number = Number(z.substring(1, tt));
+  //     console.log(z, col, row);
+  //     const value: any = worksheet[z].v;
+  //     // store header names
+  //     if (row === 1 && value) {
+  //       headers[col] = value;
+  //     }
+
+  //     if (!data[row]) data[row] = {};
+  //     data[row][headers[col]] = value;
+  //   });
+  //   // drop those first two rows which are empty
+  //   data.shift();
+  //   data.shift();
+  //   sheets[y] = data;
+  // });
   return sheets;
 };
